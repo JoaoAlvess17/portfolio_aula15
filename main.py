bf68858a37f664a6da367250a3085a420598ea4a
João Alves <81170453+JoaoAlvess17@users.noreply.github.com>
2021-05-13 16:44:35 +0100

    porfolio

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+@app.route('/portfolio/')
+def portefolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/fakebook/")
+def fakebook():
+    return render_template("fakebook.html")
+
+@app.route("/portfolio/boogle/")
+def boogle():
+    return render_template("boogle.html")
+
+@app.route("/portfolio/boogle/booglemail/")
+def booglemail():
+    return render_template("booglemail.html")
+
+@app.route("/portfolio/cabeleireiro/")
+def cabeleireiro():
+    return render_template("cabeleireiro.html")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
